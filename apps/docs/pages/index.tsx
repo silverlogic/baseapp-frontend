2791e3b80f03c05686fbb94b829c7a5fa4366d31
Filipe de Castro <fc@tsl.io>
2022-08-10 19:21:08 +0100

    update-mui-design-system

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,54 @@
-import { ButtonWithLoading } from '@baseapp-frontend/design-system-mui'
+import {
+  ButtonWithLoading,
+  PasswordField,
+  ImageUploader,
+} from '@baseapp-frontend/design-system-mui'
+import { Divider, useTheme } from '@mui/material'
+import { useState } from 'react'
 
 export default function Docs() {
+  const [images, setImages] = useState([])
+  const theme = useTheme()
   return (
     <div>
-      <h1>Docs</h1>
-      <ButtonWithLoading>button</ButtonWithLoading>
+      <h1>Components demo</h1>
+      <Divider style={{ margin: theme.spacing(2, 0) }} />
+
+      <h2>{`<ButtonWithLoading>`}</h2>
+      <div style={{ width: theme.spacing(48), display: 'flex', flexDirection: 'column' }}>
+        <ButtonWithLoading variant="contained" style={{ margin: theme.spacing(2, 0) }}>
+          button
+        </ButtonWithLoading>
+
+        <ButtonWithLoading variant="contained" loading>
+          button
+        </ButtonWithLoading>
+      </div>
+
+      <Divider style={{ margin: theme.spacing(2, 0) }} />
+
+      <div style={{ width: theme.spacing(48) }}>
+        <h2>{`<PasswordField />`}</h2>
+        <PasswordField />
+
+        <PasswordField helperText="Type your password." />
+
+        <PasswordField error helperText="Incorrect entry." />
+      </div>
+
+      <Divider style={{ margin: theme.spacing(2, 0) }} />
+
+      <h2>{`<ImageUploader />`}</h2>
+      <div style={{ width: theme.spacing(48) }}>
+        <ImageUploader
+          images={images}
+          setImages={setImages}
+          name="imageUploeader"
+          ImageProps={{ style: { outline: '1px solid blue' } }}
+          buttonLabel="Upload your image here"
+          buttonRemoveLabel="Remove"
+        />
+      </div>
     </div>
   )
 }
--- packages/design-system-mui/components/ButtonWithLoading/styled.tsx
+++ packages/design-system-mui/components/ButtonWithLoading/styled.tsx
@@ -1,6 +1,18 @@
 import { styled } from '@mui/material/styles'
-import { Button as MUIButton } from '@mui/material'
+import {
+  Button as MUIButton,
+  ButtonProps,
+  CircularProgress as MuiCircularProgress,
+  CircularProgressProps,
+} from '@mui/material'
+import { FC } from 'react'
 
-export const Button = styled(MUIButton)(({ theme }) => ({
-  background: theme.palette.primary.main,
-}))
+export const Button = styled(MUIButton)(({ theme }) => ({})) as unknown as FC<ButtonProps>
+
+export const CircularProgress = styled(MuiCircularProgress)(({ theme }) => ({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  marginTop: '-10px',
+  marginLeft: '-10px',
+})) as unknown as FC<CircularProgressProps>
