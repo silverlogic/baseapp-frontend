8bd97a42c27efa017a70e75c563c713575f8fa90
Filipe de Castro <fc@tsl.io>
2023-05-18 19:53:37 +0100

    Ba fix remove module augmentation (#37)
    * docs(changeset): Removing Module augmentation from MUI5
    
    * RELEASING: Releasing 1 package(s)
    
    Releases:
      @baseapp-frontend/design-system-mui@1.3.2
    
    [skip ci]
    
    * BA-Remove-module-augmentation

--- packages/design-system-mui/styles/theme.ts
+++ packages/design-system-mui/styles/theme.ts
@@ -1,17 +1,8 @@
 import { createTheme } from '@mui/material/styles'
 
 import { palette } from './palette'
-import { typography } from './typography'
 import { shadows } from './shadows'
-
-declare module '@mui/material/styles' {
-  interface Palette {
-    surface: typeof palette.surface
-  }
-  interface PaletteOptions {
-    surface: typeof palette.surface
-  }
-}
+import { typography } from './typography'
 
 const theme = createTheme({
   palette,
