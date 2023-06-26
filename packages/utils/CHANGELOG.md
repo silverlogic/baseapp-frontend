84a662d56e1386ead4d591e773b57f35574ec170
Alexandre Anicio <aa@tsl.io>
2023-06-26 12:00:05 -0300

    (BA) fix getApiErrorMessage types

--- packages/utils/CHANGELOG.md
+++ packages/utils/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @baseapp-frontend/utils
 
+## 1.0.2
+
+### Patch Changes
+
+- Fix `getApiErrorMessage` types.
+
 ## 1.0.1
 
 ### Patch Changes
--- packages/utils/functions/api.ts
+++ packages/utils/functions/api.ts
@@ -1,14 +1,12 @@
-import { AxiosError } from 'axios'
-
 export const getApiErrorMessage = (
-  error: AxiosError,
-  { defaultMessage = 'Something went wrong.' },
+  error: any,
+  { defaultMessage = 'Something went wrong.' } = {},
 ) => {
   let message = defaultMessage
-  if (error.message) {
+  if (error?.message) {
     message = error.message
   }
-  if (error.response?.data) {
+  if (error?.response?.data) {
     message = error.response.data[Object.keys(error.response.data)[0]] ?? message
   }
 
