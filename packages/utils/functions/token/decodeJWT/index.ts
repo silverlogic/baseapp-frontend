3975a51029ed9f92e1fe2ccc55dc3acc509eb3c4
Gadhi <gr2@tsl.io>
2023-09-26 19:36:05 -0300

    HOTFIX: Cast camelize result instead of passing generic

--- packages/utils/functions/token/decodeJWT/index.ts
+++ packages/utils/functions/token/decodeJWT/index.ts
@@ -2,4 +2,4 @@ import humps from 'humps'
 import jwt_decode from 'jwt-decode'
 
 export const decodeJWT = <JWTContentType>(token: string) =>
-  humps.camelizeKeys<JWTContentType>(jwt_decode(token))
+  humps.camelizeKeys(jwt_decode(token)) as JWTContentType
