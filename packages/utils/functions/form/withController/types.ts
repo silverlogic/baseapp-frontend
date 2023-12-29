54d1e9b54bdabd8b4c6de86117428b25fea5d869
Alexandre Anício <58861037+anicioalexandre@users.noreply.github.com>
2023-12-29 10:36:06 -0300

    BA: improve with controller types (#80)

--- packages/utils/functions/form/withController/types.ts
+++ packages/utils/functions/form/withController/types.ts
@@ -1,8 +1,10 @@
+import { ChangeEventHandler, FocusEventHandler } from 'react'
+
 import { FormControl } from '../../../types/form'
 
 type OptionalActions = {
-  onChange?: (value: any) => void
-  onBlur?: () => void
+  onChange?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
+  onBlur?: FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>
 }
 
 export type WithControllerProps<T> = FormControl & T & OptionalActions
