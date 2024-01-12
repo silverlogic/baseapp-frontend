25dc4f1fbfe7457b6d12da46766bfa7b067c871b
Filipe Oliveira <fc@tsl.io>
2024-01-12 12:14:48 +0000

    Add useInvalidateRelayStore hook

--- packages/graphql/config/index.ts
+++ packages/graphql/config/index.ts
@@ -4,3 +4,5 @@ export { default as loadSerializableQuery } from './loadSerializableQuery'
 export type * from './loadSerializableQuery'
 
 export { default as useSerializablePreloadedQuery } from './useSerializablePreloadedQuery'
+
+export { default as useInvalidateRelayStore } from './useInvalidateRelayStore'
