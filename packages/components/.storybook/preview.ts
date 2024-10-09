2890c3999dee73fd1427807513bfabf1a5e37382
deboracosilveira <deboracosilveira@gmail.com>
2024-10-09 14:54:07 -0300

    BA-1498: add action for storybook deploy

--- packages/components/.storybook/preview.ts
+++ packages/components/.storybook/preview.ts
@@ -7,8 +7,16 @@ import './overrides.css'
 
 const { initialize, mswLoader } = require('msw-storybook-addon')
 
+const isProduction = process.env.NODE_ENV === 'production'
+const serviceWorkerUrl = isProduction
+  ? '/baseapp-frontend/mockServiceWorker.js'
+  : '/mockServiceWorker.js'
+
 initialize({
   onUnhandledRequest: 'warn',
+  serviceWorker: {
+    url: serviceWorkerUrl,
+  },
 })
 
 const preview: Preview = {
