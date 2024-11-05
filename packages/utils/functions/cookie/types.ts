0bc477d1aca49bdd41e4187190d9e9a8927d8f0c
Alexandre Anicio <aa@tsl.io>
2024-11-05 10:15:46 -0300

    BA-hotfix: cookie getter and setter won't parse or stringfy as default

--- packages/utils/functions/cookie/types.ts
+++ packages/utils/functions/cookie/types.ts
@@ -1,5 +1,11 @@
+import type { CookieAttributes } from 'js-cookie'
+
 import { ServerSideRenderingOption } from '../../types/server'
 
 export interface GetCookieOptions extends ServerSideRenderingOption {
   parseJSON?: boolean
+}
+
+export interface SetCookieOptions extends CookieAttributes {
+  stringfyValue?: boolean
 }
