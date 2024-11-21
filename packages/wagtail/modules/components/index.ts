d3dcacbd0ecf08273d1eb73c86b68fe994b591c3
Hercilio Ortiz <ho@tsl.io>
2024-11-21 13:52:55 -0300

    Code review: LC review

--- packages/wagtail/modules/components/index.ts
+++ packages/wagtail/modules/components/index.ts
@@ -1,1 +1,6 @@
 export * from './Blocks'
+
+// TODO (Tech Debt): Move these exports to a dedicated index file.
+export type * from './PageTypes/types'
+export { default as PageTypes } from './PageTypes'
+export { default as StandardPage } from './PageTypes/StandardPage'
