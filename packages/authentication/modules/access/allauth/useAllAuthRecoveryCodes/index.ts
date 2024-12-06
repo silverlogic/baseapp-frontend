1f7cb98ee9d648008687dac2dcc8fc973455b019
Patrick Sluth <ps@tsl.io>
2024-12-06 08:42:32 -0700

    reauthenticate bugfix

--- packages/authentication/modules/access/allauth/useAllAuthRecoveryCodes/index.ts
+++ packages/authentication/modules/access/allauth/useAllAuthRecoveryCodes/index.ts
@@ -1,8 +1,6 @@
 'use client'
 
 import { useQuery } from '@tanstack/react-query'
-import { AxiosResponse } from 'axios'
-import { get } from 'lodash'
 
 import AllAuthApi from '../../../../services/allAuth'
 import type { UseAllAuthRecoveryCodes } from './types'
@@ -14,14 +12,6 @@ export const useAllAuthRecoveryCodes = ({
     queryFn: () => AllAuthApi.getRecoveryCodes(),
     queryKey: AllAuthApi.QUERY_KEYS.getRecoveryCodes(),
     ...getRecoveryCodesQueryOptions, // needs to be placed below all overridable options
-    retry(failureCount, error) {
-      const response = get(error as any, 'response') as AxiosResponse<any, any>
-      if (response && [401, 404].includes(response.status)) {
-        return false
-      }
-      return true
-    },
-    retryOnMount: false,
   })
 
   return {
