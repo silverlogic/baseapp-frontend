fd3a581ce1d50bd1d79c7a72558106d4b5674ee9
Pedro Tiburcio <pt@tsl.io>
2024-12-19 16:34:54 -0500

    feat: follow/unfollow and move profile components to packages

---
+++ packages/components/__generated__/FollowToggleUpdatableFragment.graphql.ts
@@ -0,0 +1,51 @@
+/**
+ * @generated SignedSource<<0c7af4c419997e9ef8ef7bf922938d7c>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+
+/* eslint-disable */
+// @ts-nocheck
+import { ReaderFragment, UpdatableFragment } from 'relay-runtime'
+import { FragmentRefs } from 'relay-runtime'
+
+export type FollowToggleUpdatableFragment$data = {
+  followersCount: number | null | undefined
+  isFollowedByMe: boolean | null | undefined
+  readonly ' $fragmentType': 'FollowToggleUpdatableFragment'
+}
+export type FollowToggleUpdatableFragment$key = {
+  readonly ' $data'?: FollowToggleUpdatableFragment$data
+  readonly $updatableFragmentSpreads: FragmentRefs<'FollowToggleUpdatableFragment'>
+}
+
+const node: ReaderFragment = {
+  argumentDefinitions: [],
+  kind: 'Fragment',
+  metadata: null,
+  name: 'FollowToggleUpdatableFragment',
+  selections: [
+    {
+      alias: null,
+      args: null,
+      kind: 'ScalarField',
+      name: 'isFollowedByMe',
+      storageKey: null,
+    },
+    {
+      alias: null,
+      args: null,
+      kind: 'ScalarField',
+      name: 'followersCount',
+      storageKey: null,
+    },
+  ],
+  type: 'Profile',
+  abstractKey: null,
+}
+
+;(node as any).hash = '4d151f5906c7d00fa429a6ccd27ca8bd'
+
+export default node
---
+++ packages/components/modules/profiles/ProfileComponent/types.ts
@@ -0,0 +1,5 @@
+import { ProfileComponentFragment$key } from '../../../__generated__/ProfileComponentFragment.graphql'
+
+export interface ProfileComponentProps {
+  profile?: ProfileComponentFragment$key | null
+}
