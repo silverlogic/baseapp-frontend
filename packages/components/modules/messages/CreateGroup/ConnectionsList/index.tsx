7a25d6397a62801e2445144878e7c5463ccc8bcc
Ronan-Fernandes <ronanf.sk@gmail.com>
2024-12-21 19:23:28 -0300

    Create groups

---
+++ packages/components/modules/messages/CreateGroup/ConnectionsList/index.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { FC } from 'react'
+
+import { LoadingState } from '@baseapp-frontend/design-system'
+
+import { Box } from '@mui/material'
+import { Virtuoso } from 'react-virtuoso'
+
+import DefaultEmptyProfilesListState from '../../EmptyProfilesListState'
+import DefaultSearchNotFoundState from '../../SearchNotFoundState'
+import { ConnectionsListProps } from './types'
+
+const ConnectionsList: FC<ConnectionsListProps> = ({
+  searchValue,
+  profiles = [],
+  isPending,
+  isLoadingNext,
+  hasNext,
+  loadNext,
+  renderItem,
+  VirtuosoProps = {},
+  EmptyProfilesListState = DefaultEmptyProfilesListState,
+  SearchNotFoundState = DefaultSearchNotFoundState,
+}) => {
+  const renderLoadingState = () => {
+    if (!isLoadingNext) return <Box sx={{ paddingTop: 3 }} />
+
+    return (
+      <LoadingState
+        sx={{ paddingTop: 3, paddingBottom: 1 }}
+        CircularProgressProps={{ size: 15 }}
+        aria-label="loading more profiles"
+      />
+    )
+  }
+
+  const emptyProfilesList = profiles.length === 0
+
+  if (!isPending && searchValue && emptyProfilesList) return <SearchNotFoundState />
+
+  if (!isPending && emptyProfilesList) return <EmptyProfilesListState />
+
+  return (
+    <Virtuoso
+      data={profiles}
+      itemContent={(_index, item) => renderItem(item)}
+      style={{ scrollbarWidth: 'none' }}
+      components={{
+        Footer: renderLoadingState,
+      }}
+      endReached={() => {
+        if (hasNext) {
+          loadNext(5)
+        }
+      }}
+      {...VirtuosoProps}
+    />
+  )
+}
+
+export default ConnectionsList
--- packages/components/modules/messages/MessagesList/MessagesGroup/types.ts
+++ packages/components/modules/messages/MessagesList/MessagesGroup/types.ts
@@ -1,16 +1,13 @@
 import { FC } from 'react'
 
-import { MessagesListFragment$data } from '../../../../__generated__/MessagesListFragment.graphql'
 import { MessageNode } from '../../types'
 import { MessageItemProps } from './MessageItem/types'
-
-type Participants = NonNullable<MessagesListFragment$data['participants']>
 
 export interface MessagesGroupProps {
   allMessages: MessageNode[]
   message: MessageNode
   messageIndex: number
-  roomParticipantsCount: Participants['totalCount']
+  isGroup?: boolean
   allMessagesLastIndex: number
   hasNext: boolean
   MessageItem?: FC<MessageItemProps>
