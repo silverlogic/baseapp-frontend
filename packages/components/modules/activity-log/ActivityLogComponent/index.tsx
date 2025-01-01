908dc94193655151bad7a3d4eb48143d1c1d7f54
mathieu bouhelier <mb@tsl.io>
2025-01-01 15:26:03 -0300

    add search feature

--- packages/components/modules/activity-log/ActivityLogComponent/index.tsx
+++ packages/components/modules/activity-log/ActivityLogComponent/index.tsx
@@ -1,11 +1,14 @@
 'use client'
 
-import { FC, useState } from 'react'
+import { FC, useState, useTransition } from 'react'
 
-import { Box } from '@mui/material'
+import { Searchbar } from '@baseapp-frontend/design-system'
+
+import { Box, Typography } from '@mui/material'
+import { useForm } from 'react-hook-form'
 
 import { ActivityLogsFragment$key } from '../../../__generated__/ActivityLogsFragment.graphql'
-import ActivityUserSearchBar from '../ActivityUserSearchBar'
+import SearchNotFoundState from '../../messages/SearchNotFoundState'
 import DateRangeChip from '../DateRangeChip'
 import EventFilterChip from '../EventFilterChip'
 import LogGroups from '../LogGroups'
@@ -13,36 +16,61 @@ import { useActivityLogs } from '../graphql/queries/ActivityLogsFragment'
 import { ActivityLogComponentProps } from './types'
 
 const ActivityLogComponent: FC<ActivityLogComponentProps> = ({ queryRef }) => {
-  const [searchTerm, setSearchTerm] = useState('')
   const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([null, null])
   const [selectedFilters, setSelectedFilters] = useState<string[]>(['All'])
+  const { control, reset, watch } = useForm({ defaultValues: { search: '' } })
+  const searchValue = watch('search')
 
+  const [isPending, startTransition] = useTransition()
   const { logGroups, loadNext, hasNext, isLoadingNext, refetch } = useActivityLogs(
     queryRef as ActivityLogsFragment$key,
   )
 
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value)
-    refetch({ count: 10, cursor: null })
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value || ''
+    startTransition(() => {
+      refetch({ userName: value, count: 10, cursor: null }, { fetchPolicy: 'store-and-network' })
+    })
   }
 
+  const handleSearchClear = () => {
+    startTransition(() => {
+      reset()
+      refetch({ userName: '' })
+    })
+  }
+  const emptyLogsList = logGroups.length === 0
+
   return (
-    <Box p={2} justifyContent="center">
-      <ActivityUserSearchBar value={searchTerm} onChange={handleSearch} />
-      <Box display="flex" mt={2} mb={2}>
-        <DateRangeChip value={dateRange} onChange={setDateRange} />
-        <EventFilterChip
-          options={['All', 'Comments', 'Reactions', 'Posts']}
-          selectedOptions={selectedFilters}
-          onChange={setSelectedFilters}
+    <Box p={2} justifyContent="center" minWidth="100%" display="flex" alignItems="center">
+      <Box justifyContent="center" maxWidth={600} width="100%" mx="auto">
+        <Typography component="h4" variant="h4" mb={4}>
+          Activity Log
+        </Typography>
+        <Searchbar
+          placeholder="Search by user"
+          name="search"
+          control={control}
+          onChange={handleSearchChange}
+          onClear={handleSearchClear}
+          isPending={isPending}
+        />
+        <Box display="flex" mt={2} mb={2}>
+          <DateRangeChip value={dateRange} onChange={setDateRange} />
+          <EventFilterChip
+            options={['All', 'Comments', 'Reactions', 'Posts']}
+            selectedOptions={selectedFilters}
+            onChange={setSelectedFilters}
+          />
+        </Box>
+        {!isPending && searchValue && emptyLogsList && <SearchNotFoundState />}
+        <LogGroups
+          logGroups={logGroups}
+          loadNext={loadNext}
+          hasNext={hasNext}
+          isLoadingNext={isLoadingNext}
         />
       </Box>
-      <LogGroups
-        logGroups={logGroups}
-        loadNext={loadNext}
-        hasNext={hasNext}
-        isLoadingNext={isLoadingNext}
-      />
     </Box>
   )
 }
