626e2a9f04626c8d10b98fbff57d619670ca22e6
Philipp Schmitt <ps2@tsl.io>
2025-01-24 10:39:17 +0100

    BA-2161 api error messages

--- packages/utils/functions/form/setFormApiErrors/index.ts
+++ packages/utils/functions/form/setFormApiErrors/index.ts
@@ -11,11 +11,22 @@ export const setFormApiErrors = <
   form: UseFormReturn<TFieldValues, TContext, TTransformedValues>,
   err: any,
 ) => {
+  let errorObject
   if (err.response?.data && typeof err.response.data === 'object') {
-    map(entries(err.response.data), ([key, errors]) => {
+    errorObject = err.response?.data
+  } else if (err.message) {
+    try {
+      errorObject = JSON.parse(err.message)
+    } catch (parsingError) {
+      // Error message is not a JSON object, no further action taken
+    }
+  }
+
+  if (errorObject) {
+    map(entries(errorObject), ([key, errors]) => {
       const fieldKey = key as Path<TFieldValues>
       const errorMessage = head(errors as string[])
-      if (!!form.getValues(fieldKey) && typeof errorMessage === 'string') {
+      if (form.getValues(fieldKey) !== undefined && typeof errorMessage === 'string') {
         form.setError(fieldKey, { type: 'manual', message: errorMessage })
       }
     })
