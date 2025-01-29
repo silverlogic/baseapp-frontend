ab545dd2c9540a2b913fc3d0acbe11c8aad5134d
luisguareschi <luisguareschi@hotmail.com>
2025-01-29 16:09:45 +0100

    Implement device logout functionality

--- packages/components/modules/devices/DeviceList/DeviceItem/DeviceActions/index.tsx
+++ packages/components/modules/devices/DeviceList/DeviceItem/DeviceActions/index.tsx
@@ -4,11 +4,26 @@ import { Button } from '@mui/material'
 
 import { DeviceActionsContainer } from './styled'
 import { DeviceActionsProps } from './types'
+import { useLogoutDevice } from '../../../queries/useLogoutDevice'
+import useNotification from '@baseapp-frontend/utils/hooks/useNotification'
+import { useRelayEnvironment } from 'react-relay'
 
-const DeviceActions: FC<DeviceActionsProps> = ({ device }) => {
+const DeviceActions: FC<DeviceActionsProps> = ({ device }) => {  
+  const environment = useRelayEnvironment()
+  const { sendApiErrorToast, sendToast } = useNotification()
+  const {mutate: logoutDevice} = useLogoutDevice({
+    onSuccess: () => {
+      sendToast('Device signed out successfully')
+      environment.commitUpdate(store => {
+        store.delete(device.id)
+      })
+    },
+    onError: (error) => sendApiErrorToast(error),
+  })
+
   return (
     <DeviceActionsContainer>
-        <Button variant="outlined" color="inherit" size="medium" sx={{ width: 'fit-content' }}>
+        <Button variant="outlined" color="inherit" size="medium" sx={{ width: 'fit-content' }} onClick={() => logoutDevice({deviceId: device.deviceId})}>
           Sign Out
         </Button>
     </DeviceActionsContainer>
