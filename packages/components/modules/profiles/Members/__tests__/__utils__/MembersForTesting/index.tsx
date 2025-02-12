123543ed1fc1ec09c50a2caa5151b9068abeaffc
joaosoutto <joaosoutto92@gmail.com>
2025-02-12 09:22:15 -0500

    migrating members tests

--- packages/components/modules/profiles/Members/__tests__/__utils__/MembersForTesting/index.tsx
+++
@@ -1,9 +0,0 @@
-import withProviders from '../../../../../../__test_utils__/withProviders'
-import Members from '../../../index'
-import { UserMembersProps } from '../../../types'
-
-const MembersForTesting = (props?: Partial<UserMembersProps>) => {
-  return <Members subtitle="Members Testing" {...props} />
-}
-
-export default withProviders(MembersForTesting)
