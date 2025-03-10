62769497a07abcd81d8f057c91dfcb8aaab4ca0c
Sunday <sundayguru@gmail.com>
2025-03-10 19:16:10 +0100

    BA-2357: Support first and last name during signup

--- packages/authentication/modules/access/useSignUp/constants.ts
+++ packages/authentication/modules/access/useSignUp/constants.ts
@@ -4,8 +4,17 @@ import { z } from 'zod'
 
 import type { RegisterRequest } from '../../../types/auth'
 
-export const DEFAULT_VALIDATION_SCHEMA = z.object({
+export const DEFAULT_VALIDATION_SCHEMA_WITH_NAME = z.object({
   name: z.string().min(1, ZOD_MESSAGE.required),
+  password: z.string().min(1, ZOD_MESSAGE.required).regex(PASSWORD_REGEX, {
+    message: ZOD_MESSAGE.password,
+  }),
+  email: z.string().min(1, ZOD_MESSAGE.required).email(ZOD_MESSAGE.email),
+})
+
+export const DEFAULT_VALIDATION_SCHEMA = z.object({
+  firstName: z.string().min(1, ZOD_MESSAGE.required),
+  lastName: z.string().min(1, ZOD_MESSAGE.required),
   password: z.string().min(1, ZOD_MESSAGE.required).regex(PASSWORD_REGEX, {
     message: ZOD_MESSAGE.password,
   }),
@@ -14,6 +23,8 @@ export const DEFAULT_VALIDATION_SCHEMA = z.object({
 
 export const DEFAULT_INITIAL_VALUES: RegisterRequest = {
   name: '',
+  firstName: '',
+  lastName: '',
   email: '',
   password: '',
 }
