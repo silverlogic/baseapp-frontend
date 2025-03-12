fcefd834a66b578b66db14e8c31ec50aad3d28da
Pedro Tiburcio <pt@tsl.io>
2025-03-12 14:07:36 -0400

    feat: add AlertTriangleIcon to native

--- packages/authentication/modules/access/useChangePassword/__tests__/useChangePassword.test.tsx
+++ packages/authentication/modules/access/useChangePassword/__tests__/useChangePassword.test.tsx
@@ -11,6 +11,7 @@ import useChangePassword from '../index'
 
 describe('useChangePassword', () => {
   const currentPassword = '1234'
+<<<<<<< HEAD
 <<<<<<< HEAD:packages/authentication/modules/access/useChangeExpiredPassword/__tests__/useChangeExpiredPassword.test.tsx
   const password = '12345#Abcde'
   const token = 'fake-token'
@@ -18,6 +19,10 @@ describe('useChangePassword', () => {
   const password = '123456'
 >>>>>>> 3034abb (feat: migrate change password from baseapp-frontend-template):packages/authentication/modules/access/useChangePassword/__tests__/useChangePassword.test.tsx
   const changePasswordUrl = '/change-expired-password'
+=======
+  const password = 'abcABC@123456'
+  const changePasswordUrl = '/users/change-password'
+>>>>>>> 426f513 (feat: add AlertTriangleIcon to native)
 
   afterEach(() => {
     ;(global.fetch as jest.Mock).mockClear() // Clear the mock between tests
@@ -170,7 +175,7 @@ describe('useChangePassword', () => {
 
 describe('useChangePassword with token for expired passwords', () => {
   const currentPassword = '1234'
-  const password = '123456'
+  const password = 'abcABC@123456'
   const token = 'fake-token'
   const changePasswordUrl = '/change-expired-password'
 
