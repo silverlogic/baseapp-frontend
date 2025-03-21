6756f3cf932e205361f774918c42d649392dafab
David Bermudez <db@tsl.io>
2025-03-21 17:57:11 -0400

    Content Feed New Post

---
+++ packages/components/modules/content-feed/web/NewContentPost/types.ts
@@ -0,0 +1,10 @@
+export interface ContentPostCreateForm {
+  content: string
+  images?: string[] | File[] | Blob[]
+}
+
+export type UploadableContentPostFiles = {
+  [key: `image-${number}`]: File | Blob
+}
+
+export interface INewContentPostProps {}
