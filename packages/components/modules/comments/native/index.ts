6e4b3096994e641d111ec3ac7cb9b25c06483898
Philipp Schmitt <ps2@tsl.io>
2025-03-27 10:46:16 +0100

    BA-2399 Remove unnecessary comments

--- packages/components/modules/comments/native/index.ts
+++ packages/components/modules/comments/native/index.ts
@@ -3,17 +3,11 @@
 export { default as CommentCreate } from './CommentCreate'
 export type * from './CommentCreate/types'
 
-// export { default as CommentItem } from './CommentItem'
-// export type * from './CommentItem/types'
-
-// export { default as CommentUpdate } from './CommentUpdate'
-// export type * from './CommentUpdate/types'
+export { default as CommentItem } from './CommentItem'
+export type * from './CommentItem/types'
 
 export { default as Comments } from './Comments'
 export type * from './Comments/types'
 
 export { default as CommentsList } from './CommentsList'
 export type * from './CommentsList/types'
-
-// export { default as CommentsReplies } from './CommentItem/CommentsReplies'
-// export type * from './CommentItem/CommentsReplies/types'
