c68db9bef6ce2246e923e0bcc0b0797e1580d7fd
Rafael Rocha <rr@tsl.io>
2025-03-27 11:29:51 +0100

    BA-2387: activity log storybook

---
+++ packages/components/modules/activity-log/web/ActivityLogComponent/__storybook__/mockResolvers.ts
@@ -0,0 +1,308 @@
+export const mockData = {
+  data: {
+    activityLogs: {
+      edges: [
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6NWQ1NDA4M2UtZjRkOS00MzQ4LWJmM2ItN2YwMzBiZjgwYThl',
+            createdAt: '2025-03-09T10:11:00.395806+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjozOQ==',
+              fullName: 'Activity Lok',
+              email: 'll+act2@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/2162b9f5-3c50-4b45-8d33-9fdb28f5dee4.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjc0',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6MTY4Yzc1NWEtMTlkYy00MGE1LWFhYzQtNDcyYzZlMDRiNjJj',
+            createdAt: '2025-03-09T10:11:03.924627+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjozOQ==',
+              fullName: 'Activity Lok',
+              email: 'll+act2@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/2162b9f5-3c50-4b45-8d33-9fdb28f5dee4.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjc1',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6ZmZkMGNkODMtYmE4MC00OTI4LWI4NTUtOTkwNGZiMWVmM2Mz',
+            createdAt: '2025-03-09T10:11:10.018741+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjozOQ==',
+              fullName: 'Activity Lok',
+              email: 'll+act2@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/2162b9f5-3c50-4b45-8d33-9fdb28f5dee4.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjc2',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6N2UxZGE0ZDgtMjlhNy00MjFkLThiNzQtOWI2MDYwZjU3M2Zi',
+            createdAt: '2025-02-11T18:41:02.537558+00:00',
+            verb: 'comments.pin_comment',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjo4',
+              fullName: 'Lokman Hekim ',
+              email: 'll@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjMw',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6NzhmMTk2NGQtMmNlYy00ZGM5LTgzOTMtOWQxODBmODE4ODEw',
+            createdAt: '2025-02-11T18:41:09.715742+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjo4',
+              fullName: 'Lokman Hekim ',
+              email: 'll@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjMx',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6M2RkY2QyNjUtODcxMi00NjdjLThkNTYtNTg4MDJlZjc0ZDYz',
+            createdAt: '2025-02-11T18:41:33.898957+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjo4',
+              fullName: 'Lokman Hekim ',
+              email: 'll@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjMy',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6MmZhMDE0NGUtMTlkOS00ZGMyLTk5MzEtN2FiNzBjMDk2OWJk',
+            createdAt: '2025-02-11T18:41:28.723774+00:00',
+            verb: 'comments.add_comment',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjo4',
+              fullName: 'Lokman Hekim ',
+              email: 'll@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjMz',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6MGI5YmVmY2MtZDE5NS00M2Q2LWE1NjgtYjA3ZDQ3MTVkMjJh',
+            createdAt: '2025-02-14T18:11:31.650843+00:00',
+            verb: 'comments.add_comment',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjo4',
+              fullName: 'Lokman Hekim ',
+              email: 'll@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjM1',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6N2UxYzY1ZDctNjJiZS00Mjg0LTk5NzMtN2Y4MzE4ZWUyYzY3',
+            createdAt: '2025-02-11T23:49:59.702665+00:00',
+            verb: 'comments.add_comment',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjoyNA==',
+              fullName: 'Biff Wong',
+              email: 'bw+LOTU@tsl.io',
+              avatar: null,
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjM4',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6MGEyYzJhYzEtMTM4NS00MTUyLWIzYWEtZjgwMDkwMjI2NmQ4',
+            createdAt: '2025-02-11T23:50:02.925615+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjoyNA==',
+              fullName: 'Biff Wong',
+              email: 'bw+LOTU@tsl.io',
+              avatar: null,
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjM5',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6YzhhNmIyNGYtNGFhYS00NTE0LThhZDEtYTMwNDllOTE4ODVh',
+            createdAt: '2025-02-12T00:15:48.207544+00:00',
+            verb: 'comments.add_comment',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjoyNA==',
+              fullName: 'Biff Wong',
+              email: 'bw+LOTU@tsl.io',
+              avatar: null,
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjQw',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6NDFjN2NiNjgtOWNkMi00MTFlLWI0NDgtMDU4ZWQ4ODk0NDc1',
+            createdAt: '2025-02-12T00:15:50.184763+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjoyNA==',
+              fullName: 'Biff Wong',
+              email: 'bw+LOTU@tsl.io',
+              avatar: null,
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjQx',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6YWVlZWUxNjMtNDZhNy00YmNmLTg1ZWUtNjQwZDRiNjk1NzZk',
+            createdAt: '2025-02-14T18:10:00.699304+00:00',
+            verb: 'comments.add_comment',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjo4',
+              fullName: 'Lokman Hekim ',
+              email: 'll@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjQy',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6NGMwNDBjN2YtNzU1OC00OTlmLWI1NmItNTg0MTllZDU3NjRm',
+            createdAt: '2025-02-14T18:10:10.056471+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjo4',
+              fullName: 'Lokman Hekim ',
+              email: 'll@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjQz',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6NDdjMTA0MTYtMzgwYS00ZGJlLTg3NjgtYmYzNTRjNzkwNDdk',
+            createdAt: '2025-02-14T18:10:12.503969+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjo4',
+              fullName: 'Lokman Hekim ',
+              email: 'll@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjQ0',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6ZTE5ZmE2YzMtZjIxOC00OWVjLTljZTQtZGRjYzA5N2U1MmQ4',
+            createdAt: '2025-02-14T18:10:15.121559+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjo4',
+              fullName: 'Lokman Hekim ',
+              email: 'll@tsl.io',
+              avatar: {
+                url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
+              },
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjQ1',
+        },
+        {
+          node: {
+            id: 'QWN0aXZpdHlMb2c6YzExMTViM2YtNjVmOC00Y2M3LThjNTYtNzg3OWQ0ZGFhNGQy',
+            createdAt: '2025-03-06T14:51:56.218344+00:00',
+            verb: 'baseapp_reactions.add_reaction',
+            url: '/graphql',
+            user: {
+              id: 'VXNlcjox',
+              fullName: 'ap@tsl.io',
+              email: 'ap@tsl.io',
+              avatar: null,
+            },
+            __typename: 'ActivityLog',
+          },
+          cursor: 'YXJyYXljb25uZWN0aW9uOjY2',
+        },
+      ],
+      pageInfo: {
+        endCursor: 'YXJyYXljb25uZWN0aW9uOjkz',
+        hasNextPage: false,
+      },
+    },
+  },
+}
