deb6e31760d608d45dbac0063dfff8f6b95f8395
Rafael Rocha <rr@tsl.io>
2025-03-31 09:59:14 +0200

    randomize info in Storybook

--- packages/components/modules/activity-log/web/ActivityLogComponent/__storybook__/mockResolvers.ts
+++ packages/components/modules/activity-log/web/ActivityLogComponent/__storybook__/mockResolvers.ts
@@ -4,14 +4,14 @@ export const mockData = {
       edges: [
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6NWQ1NDA4M2UtZjRkOS00MzQ4LWJmM2ItN2YwMzBiZjgwYThl',
+            id: 'abcdef1234567890',
             createdAt: '2025-03-09T10:11:00.395806+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjozOQ==',
-              fullName: 'Activity Lok',
-              email: 'll+act2@tsl.io',
+              id: '1234',
+              fullName: 'Activity User',
+              email: 'test@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/2162b9f5-3c50-4b45-8d33-9fdb28f5dee4.JPG.48x48_q85.jpg',
               },
@@ -22,14 +22,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6MTY4Yzc1NWEtMTlkYy00MGE1LWFhYzQtNDcyYzZlMDRiNjJj',
+            id: 'abcd1234',
             createdAt: '2025-03-09T10:11:03.924627+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjozOQ==',
-              fullName: 'Activity Lok',
-              email: 'll+act2@tsl.io',
+              id: '5678',
+              fullName: 'Activity User',
+              email: 'test@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/2162b9f5-3c50-4b45-8d33-9fdb28f5dee4.JPG.48x48_q85.jpg',
               },
@@ -40,14 +40,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6ZmZkMGNkODMtYmE4MC00OTI4LWI4NTUtOTkwNGZiMWVmM2Mz',
+            id: 'efgh5678',
             createdAt: '2025-03-09T10:11:10.018741+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjozOQ==',
-              fullName: 'Activity Lok',
-              email: 'll+act2@tsl.io',
+              id: '91011',
+              fullName: 'Activity User',
+              email: 'test@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/2162b9f5-3c50-4b45-8d33-9fdb28f5dee4.JPG.48x48_q85.jpg',
               },
@@ -58,14 +58,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6N2UxZGE0ZDgtMjlhNy00MjFkLThiNzQtOWI2MDYwZjU3M2Zi',
+            id: 'ijkl9012',
             createdAt: '2025-02-11T18:41:02.537558+00:00',
             verb: 'comments.pin_comment',
             url: '/graphql',
             user: {
-              id: 'VXNlcjo4',
-              fullName: 'Lokman Hekim ',
-              email: 'll@tsl.io',
+              id: '1213',
+              fullName: 'Test User',
+              email: 'test2@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
               },
@@ -76,14 +76,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6NzhmMTk2NGQtMmNlYy00ZGM5LTgzOTMtOWQxODBmODE4ODEw',
+            id: 'mnop3456',
             createdAt: '2025-02-11T18:41:09.715742+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjo4',
-              fullName: 'Lokman Hekim ',
-              email: 'll@tsl.io',
+              id: '1415',
+              fullName: 'Test User',
+              email: 'test2@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
               },
@@ -94,14 +94,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6M2RkY2QyNjUtODcxMi00NjdjLThkNTYtNTg4MDJlZjc0ZDYz',
+            id: 'qrst6789',
             createdAt: '2025-02-11T18:41:33.898957+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjo4',
-              fullName: 'Lokman Hekim ',
-              email: 'll@tsl.io',
+              id: '1617',
+              fullName: 'Test User',
+              email: 'test2@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
               },
@@ -112,14 +112,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6MmZhMDE0NGUtMTlkOS00ZGMyLTk5MzEtN2FiNzBjMDk2OWJk',
+            id: 'uvwx9012',
             createdAt: '2025-02-11T18:41:28.723774+00:00',
             verb: 'comments.add_comment',
             url: '/graphql',
             user: {
-              id: 'VXNlcjo4',
-              fullName: 'Lokman Hekim ',
-              email: 'll@tsl.io',
+              id: '1819',
+              fullName: 'Test User',
+              email: 'test2@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
               },
@@ -130,14 +130,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6MGI5YmVmY2MtZDE5NS00M2Q2LWE1NjgtYjA3ZDQ3MTVkMjJh',
+            id: 'yzab1234',
             createdAt: '2025-02-14T18:11:31.650843+00:00',
             verb: 'comments.add_comment',
             url: '/graphql',
             user: {
-              id: 'VXNlcjo4',
-              fullName: 'Lokman Hekim ',
-              email: 'll@tsl.io',
+              id: '2021',
+              fullName: 'Test User',
+              email: 'test2@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
               },
@@ -148,14 +148,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6N2UxYzY1ZDctNjJiZS00Mjg0LTk5NzMtN2Y4MzE4ZWUyYzY3',
+            id: 'cdef5678',
             createdAt: '2025-02-11T23:49:59.702665+00:00',
             verb: 'comments.add_comment',
             url: '/graphql',
             user: {
-              id: 'VXNlcjoyNA==',
-              fullName: 'Biff Wong',
-              email: 'bw+LOTU@tsl.io',
+              id: '2223',
+              fullName: 'Random user',
+              email: 'test4@tsl.io',
               avatar: null,
             },
             __typename: 'ActivityLog',
@@ -164,14 +164,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6MGEyYzJhYzEtMTM4NS00MTUyLWIzYWEtZjgwMDkwMjI2NmQ4',
+            id: 'ghij6789',
             createdAt: '2025-02-11T23:50:02.925615+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjoyNA==',
-              fullName: 'Biff Wong',
-              email: 'bw+LOTU@tsl.io',
+              id: '2425',
+              fullName: 'Random user',
+              email: 'test4@tsl.io',
               avatar: null,
             },
             __typename: 'ActivityLog',
@@ -180,14 +180,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6YzhhNmIyNGYtNGFhYS00NTE0LThhZDEtYTMwNDllOTE4ODVh',
+            id: 'ijkl9012',
             createdAt: '2025-02-12T00:15:48.207544+00:00',
             verb: 'comments.add_comment',
             url: '/graphql',
             user: {
-              id: 'VXNlcjoyNA==',
-              fullName: 'Biff Wong',
-              email: 'bw+LOTU@tsl.io',
+              id: '2627',
+              fullName: 'Random user',
+              email: 'test4@tsl.io',
               avatar: null,
             },
             __typename: 'ActivityLog',
@@ -196,14 +196,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6NDFjN2NiNjgtOWNkMi00MTFlLWI0NDgtMDU4ZWQ4ODk0NDc1',
+            id: 'mnop1234',
             createdAt: '2025-02-12T00:15:50.184763+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjoyNA==',
-              fullName: 'Biff Wong',
-              email: 'bw+LOTU@tsl.io',
+              id: '2829',
+              fullName: 'Random user',
+              email: 'test4@tsl.io',
               avatar: null,
             },
             __typename: 'ActivityLog',
@@ -212,14 +212,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6YWVlZWUxNjMtNDZhNy00YmNmLTg1ZWUtNjQwZDRiNjk1NzZk',
+            id: 'qrst5678',
             createdAt: '2025-02-14T18:10:00.699304+00:00',
             verb: 'comments.add_comment',
             url: '/graphql',
             user: {
-              id: 'VXNlcjo4',
-              fullName: 'Lokman Hekim ',
-              email: 'll@tsl.io',
+              id: '3031',
+              fullName: 'Test User',
+              email: 'test2@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
               },
@@ -230,14 +230,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6NGMwNDBjN2YtNzU1OC00OTlmLWI1NmItNTg0MTllZDU3NjRm',
+            id: 'stuv9012',
             createdAt: '2025-02-14T18:10:10.056471+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjo4',
-              fullName: 'Lokman Hekim ',
-              email: 'll@tsl.io',
+              id: '3233',
+              fullName: 'Test User',
+              email: 'test2@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
               },
@@ -248,14 +248,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6NDdjMTA0MTYtMzgwYS00ZGJlLTg3NjgtYmYzNTRjNzkwNDdk',
+            id: 'wxyz1234',
             createdAt: '2025-02-14T18:10:12.503969+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjo4',
-              fullName: 'Lokman Hekim ',
-              email: 'll@tsl.io',
+              id: '3435',
+              fullName: 'Test User',
+              email: 'test2@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
               },
@@ -266,14 +266,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6ZTE5ZmE2YzMtZjIxOC00OWVjLTljZTQtZGRjYzA5N2U1MmQ4',
+            id: 'zabc5678',
             createdAt: '2025-02-14T18:10:15.121559+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjo4',
-              fullName: 'Lokman Hekim ',
-              email: 'll@tsl.io',
+              id: '3637',
+              fullName: 'Test User',
+              email: 'test2@tsl.io',
               avatar: {
                 url: 'https://nyc3.digitaloceanspaces.com/baseapp-db-staging/media/profile_images/7069d845-181f-45c8-ab45-ca10c0f7937f.JPG.48x48_q85.jpg',
               },
@@ -284,14 +284,14 @@ export const mockData = {
         },
         {
           node: {
-            id: 'QWN0aXZpdHlMb2c6YzExMTViM2YtNjVmOC00Y2M3LThjNTYtNzg3OWQ0ZGFhNGQy',
+            id: 'defg6789',
             createdAt: '2025-03-06T14:51:56.218344+00:00',
             verb: 'baseapp_reactions.add_reaction',
             url: '/graphql',
             user: {
-              id: 'VXNlcjox',
-              fullName: 'ap@tsl.io',
-              email: 'ap@tsl.io',
+              id: '3839',
+              fullName: 'test3@tsl.io',
+              email: 'test3@tsl.io',
               avatar: null,
             },
             __typename: 'ActivityLog',
