73a8654ca48031d8014964eedb65eec21fe448ce
Lúcio BJ <lb@tsl.com>
2025-06-26 17:31:14 -0300

    Added missing types

--- packages/components/modules/payments/web/InvoiceListTable/types.ts
+++ packages/components/modules/payments/web/InvoiceListTable/types.ts
@@ -1,8 +1,9 @@
-import { TableProps, TableRowProps, TypographyProps } from '@mui/material'
+import { TableCellProps, TableProps, TableRowProps, TypographyProps } from '@mui/material'
 
 export interface InvoiceListTableProps {
   title?: string
   titleProps?: TypographyProps
   tableProps?: TableProps
   rowProps?: TableRowProps
+  cellProps?: TableCellProps
 }
