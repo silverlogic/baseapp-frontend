efa0c30ef3f254bca71017bc11c3f6fb57006203
mathieu bouhelier <mathieubouhelier@gmail.com>
2025-08-26 11:05:09 -0300

    add check on hasUser

--- packages/components/modules/comments/web/CommentItem/index.tsx
+++ packages/components/modules/comments/web/CommentItem/index.tsx
@@ -64,7 +64,7 @@ const CommentItem: FC<CommentItemProps> = ({
   const [deleteComment, isDeletingComment] = useCommentDeleteMutation()
 
   const profileUrl = `${profilePath}/${comment?.user?.pk}`
-  const hasUser = Boolean(comment.user)
+  const hasUser = Boolean(comment?.user)
 
   const showReplies = () => {
     if (isReplyExpanded) return
