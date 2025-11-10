c3d32d9acbe5db55b6346e7ecf761047503a52a7
Gabriel Santos <gs@tsl.io>
2025-11-10 15:08:46 -0300

    fix

--- packages/components/modules/messages/native/index.ts
+++ packages/components/modules/messages/native/index.ts
@@ -1,6 +1,8 @@
 // export native messages components
 export * from './graphql/subscriptions/useMessagesListSubscription'
 export * from './graphql/subscriptions/useRoomListSubscription'
+export * from './graphql/queries/CreateRoomPageQuery'
+
 export { default as ChatRooms } from './ChatRooms'
 export { default as RoomsList } from './RoomsList'
 export { default as CreateRoomList } from './SingleChatCreate/CreateRoomList'
