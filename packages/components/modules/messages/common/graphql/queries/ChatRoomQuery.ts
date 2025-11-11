fac96cd1bd26f0cd2732d5a8c955e99830cef9da
Gabriel Santos <gs@tsl.io>
2025-11-11 19:26:02 -0300

    fix-comments

--- packages/components/modules/messages/common/graphql/queries/ChatRoomQuery.ts
+++ packages/components/modules/messages/common/graphql/queries/ChatRoomQuery.ts
@@ -3,11 +3,6 @@ import { graphql } from 'react-relay'
 export const ChatRoomQuery = graphql`
   query ChatRoomQuery($roomId: ID!) {
     chatRoom(id: $roomId) {
-      id
-      isArchived
-      participantsCount
-      ...TitleFragment
-      ...MessagesListFragment
       ...ChatRoomFragment
     }
   }
