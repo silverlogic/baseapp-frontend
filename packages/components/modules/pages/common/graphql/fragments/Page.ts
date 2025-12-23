b18d1b36ce486c0f200ed89089a419868d90e4ec
Hercilio M. Ortiz <herciliomartins@gmail.com>
2025-12-23 09:13:09 -0300

    Bufix: Adding id to page fragment

--- packages/components/modules/pages/common/graphql/fragments/Page.ts
+++ packages/components/modules/pages/common/graphql/fragments/Page.ts
@@ -2,6 +2,7 @@ import { graphql } from 'react-relay'
 
 export const PageComponentFragment = graphql`
   fragment PageComponentFragment on Page {
+    id
     title
     body
     canChange: hasPerm(perm: "change")
