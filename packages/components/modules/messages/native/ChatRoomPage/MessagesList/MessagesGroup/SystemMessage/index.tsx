2981815517cd182346e3f7adef3f5250b525fbda
David Bermudez <db@tsl.io>
2026-01-07 15:00:58 -0500

    feature/BA-2930-rn-messages-group-create-new-group-chat

--- packages/components/modules/messages/native/ChatRoomPage/MessagesList/MessagesGroup/SystemMessage/index.tsx
+++ packages/components/modules/messages/native/ChatRoomPage/MessagesList/MessagesGroup/SystemMessage/index.tsx
@@ -2,15 +2,26 @@ import { FC } from 'react'
 
 import { MessageItemFragment } from '@baseapp-frontend/components/messages/common'
 import { Text } from '@baseapp-frontend/design-system/components/native/typographies'
+import { View } from '@baseapp-frontend/design-system/components/native/views'
+import { useTheme } from '@baseapp-frontend/design-system/providers/native'
 
 import { useFragment } from 'react-relay'
 
+import { createStyles } from './styles'
 import { SystemMessageProps } from './types'
 
 const SystemMessage: FC<SystemMessageProps> = ({ messageRef }) => {
+  const theme = useTheme()
+  const styles = createStyles(theme)
   const message = useFragment(MessageItemFragment, messageRef)
 
-  return <Text variant="caption">{message.content}</Text>
+  return (
+    <View style={styles.wrapper}>
+      <Text variant="caption" style={styles.text}>
+        {message.content}
+      </Text>
+    </View>
+  )
 }
 
 export { SystemMessage }
--- packages/design-system/components/native/appbars/AppBar/types.ts
+++ packages/design-system/components/native/appbars/AppBar/types.ts
@@ -6,6 +6,9 @@ export type AppBarProps = {
   title?: string
   titleComponent?: React.ReactNode
   onBack?: () => void
+  onNext?: () => void
+  nextLabel?: string
+  nextIcon?: React.ReactNode
   onClose?: () => void
   BackIcon?: FC<SvgIconProps>
   CloseIcon?: FC<SvgIconProps>
