f4bf128fc6cef1d412d1b97d7cf7c0a2f9edb9a2
Lúcio BJ <lb@tsl.io>
2026-01-07 15:19:13 -0300

    Add canReply boolean on comment

--- packages/components/modules/comments/common/graphql/queries/CommentItem.ts
+++ packages/components/modules/comments/common/graphql/queries/CommentItem.ts
@@ -30,6 +30,7 @@ export const CommentItemFragmentQuery = graphql`
       total
     }
 
+    canReply
     canChange: hasPerm(perm: "change")
     canDelete: hasPerm(perm: "delete")
     canReport: hasPerm(perm: "report")
