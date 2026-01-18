35da9ba7e9a096ee2ac07d22ef7148b03609c37e
Rodean Fraser <rf2@tsl.io>
2026-01-17 22:16:53 -0500

    profile settings tab permissions check

--- packages/components/modules/profiles/common/graphql/queries/Profile.ts
+++ packages/components/modules/profiles/common/graphql/queries/Profile.ts
@@ -3,6 +3,7 @@ import { graphql } from 'react-relay'
 export const ProfileByIdQuery = graphql`
   query ProfileByIdQuery($id: ID!) {
     profile(id: $id) {
+      canChange: hasPerm(perm: "change")
       ...ProfileComponentFragment
       ...FollowToggleUpdatableFragment
     }
