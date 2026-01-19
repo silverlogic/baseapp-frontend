aeb4dc181df6561c5210d7b11a25b444a04fd86b
Lúcio BJ <lbjunq@hotmail.com>
2026-01-19 10:58:01 -0300

    feature/BA-2940-rn-edit-comment (#317)
    ✅ RN - Edit a Comment
    Key details
    Description
    
    Acceptance Criteria
    
    Given that a user has created a comment, when they press and hold the
    comment, then a drawer should slide up from the bottom displaying the
    comment options menu.
    
            Implement a backdrop when the drawer comes up.
    
    When the user selects the Edit Comment option from the menu, then an
    input box should open, pre-filled with the current comment content.
    
    The input box should label informing the user that they are editing an
    existing comment.
    
    Given that the user is editing a comment, when they click the "X" on the
    label, then the edit should be canceled, and the comment should remain
    unchanged.
    
    Given that the user has finished editing the comment, when they press
    the send button, then the updated comment should be saved.
    
    When a comment has been edited and saved, then an "Edited" label should
    appear next to it, indicating that the original content has been
    modified.
    
    Design Link:
    https://www.figma.com/design/z5ZdVlKEMmGIX4GVnjwfxA/BaseApp---NATIVE?node-id=7245-44177&t=pkyMShfd1d7IXixb-0
    
    Approvd
    https://app.approvd.io/silverlogic/BA/stories/39784
    
    Demo: https://www.loom.com/share/76dd3129d79a4a0fa2b35973b38ad3cd
    
    ---------
    
    Co-authored-by: Lúcio BJ <lb@tsl.io>

--- packages/components/modules/comments/native/CommentItem/index.tsx
+++ packages/components/modules/comments/native/CommentItem/index.tsx
@@ -4,6 +4,7 @@ import { AvatarWithPlaceholder } from '@baseapp-frontend/design-system/component
 import { Text } from '@baseapp-frontend/design-system/components/native/typographies'
 import { View } from '@baseapp-frontend/design-system/components/native/views'
 
+import { Pressable } from 'react-native'
 import { useRefetchableFragment } from 'react-relay'
 
 import { CommentItemRefetchQuery } from '../../../../__generated__/CommentItemRefetchQuery.graphql'
@@ -17,6 +18,7 @@ import { CommentItemProps } from './types'
 
 const CommentItem: FC<CommentItemProps> = ({
   comment: commentRef,
+  onLongPress,
   Timestamp = DefaultTimestamp,
   CommentReplyButton = DefaultCommentReplyButton,
   CommentReactionButton = DefaultCommentReactionButton,
@@ -26,43 +28,54 @@ const CommentItem: FC<CommentItemProps> = ({
     commentRef,
   )
 
+  const styles = createStyles()
+
   const replyToComment = () => {
     // TODO (in another story)
     console.log('Reply button clicked')
   }
 
-  const styles = createStyles()
-
   if (!comment) {
     return null
   }
+
   return (
-    <View style={styles.rootContainer}>
-      <View style={styles.avatarContainer}>
-        <AvatarWithPlaceholder imgSource={comment.user?.avatar?.url} />
-      </View>
-      <View style={styles.bodyContainer}>
-        <Text variant="subtitle2" color="high">
-          {comment.user?.fullName}
-        </Text>
-        <Text variant="body2" color="high">
-          {comment.body}
-        </Text>
-        <View style={styles.footerContainer}>
-          <View style={styles.buttonContainer}>
-            <CommentReactionButton target={comment} shouldUseBottomSheetSafeComponents />
-            <CommentReplyButton
-              onReply={replyToComment}
-              commentId={comment.id}
-              shouldUseBottomSheetSafeComponents
-            />
-          </View>
-          <View style={styles.timestampContainer}>
-            <Timestamp date={comment.created} />
+    <Pressable onLongPress={() => onLongPress?.(comment)}>
+      <View style={styles.rootContainer}>
+        <View style={styles.avatarContainer}>
+          <AvatarWithPlaceholder imgSource={comment.user?.avatar?.url} />
+        </View>
+        <View style={styles.bodyContainer}>
+          <Text variant="subtitle2" color="high">
+            {comment.user?.fullName}
+          </Text>
+          <Text variant="body2" color="high">
+            {comment.body}
+            {comment.isEdited ? (
+              <Text variant="body2" color="disabled" style={{ fontSize: 12 }}>
+                {' '}
+                (edited)
+              </Text>
+            ) : (
+              ''
+            )}
+          </Text>
+          <View style={styles.footerContainer}>
+            <View style={styles.buttonContainer}>
+              <CommentReactionButton target={comment} shouldUseBottomSheetSafeComponents />
+              <CommentReplyButton
+                onReply={replyToComment}
+                commentId={comment.id}
+                shouldUseBottomSheetSafeComponents
+              />
+            </View>
+            <View style={styles.timestampContainer}>
+              <Timestamp date={comment.created} />
+            </View>
           </View>
         </View>
       </View>
-    </View>
+    </Pressable>
   )
 }
 
