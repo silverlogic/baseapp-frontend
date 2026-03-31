a65964e39dfda39ca3bfb75ca57bddd467fb0eee
Alexandre Anício <aa@tsl.io>
2026-03-31 14:12:00 -0300

    feat: customize reaction button

--- packages/components/modules/comments/web/CommentItem/CommentReactionButton/types.ts
+++ packages/components/modules/comments/web/CommentItem/CommentReactionButton/types.ts
@@ -1,5 +1,8 @@
+import type { MouseEvent } from 'react'
+
 import type { ReactionButton_target$key } from '../../../../../__generated__/ReactionButton_target.graphql'
 
 export interface CommentReactionButtonProps {
   target: ReactionButton_target$key
+  onClick?: (e: MouseEvent) => void
 }
