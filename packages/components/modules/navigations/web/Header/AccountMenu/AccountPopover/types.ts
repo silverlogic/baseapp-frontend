2aaf0f49ed561e81f47661f968124a42b4ed267b
Matheus Domingos <md@tsl.io>
2026-04-15 23:09:30 -0300

    fix: update imports for FileSystem to use legacy version

--- packages/components/modules/navigations/web/Header/AccountMenu/AccountPopover/types.ts
+++ packages/components/modules/navigations/web/Header/AccountMenu/AccountPopover/types.ts
@@ -1,11 +1,11 @@
 import type { FC, ReactElement } from 'react'
 
 import type { CSSProperties } from '@mui/material/styles/createMixins'
+import { TypographyProps } from '@mui/material/Typography/Typography'
 
 import type { AddProfileMenuItemProps, ProfilesListProps } from '../../../../../profiles/web'
 import { CurrentProfileMenuProps } from './CurrentProfileMenu/types'
 import type { LogoutItemProps } from './LogoutItem/types'
-import { TypographyProps } from '@mui/material/Typography/Typography'
 
 export interface AccountPopoverProps
   extends Omit<CurrentProfileMenuProps, 'handlePopoverOnClose' | 'setOpenProfilesList'> {
